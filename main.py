48384634fc3ecb068bf2143e5a53c2c3c312a458
Estima01 <matheus.estima@gmail.com>
2022-10-08 15:12:41 -0300

    inserindo itens e mostrando

--- main.py
+++ main.py
@@ -1,36 +1,57 @@
-from turtle import heading
 import PySimpleGUI as sg
 
+produto = []
+quantidade = []
+preco = []
 contact_information = []
-#gestor de estoque
 
-
-layout = [[sg.Text('Estoque')], [sg.Table(values=contact_information, 
-headings=['Produto','Quantidade','Preço'],
-justification='center',
-num_rows=20,
-alternating_row_color='lightblue',
-key='-TABLE-', tooltip='This is a table')],
-[sg.Button('Cadastrar'), sg.Button('Editar'), sg.Button('Excluir'), sg.Button('Sair')]]
-              
-window = sg.Window('+Supplier', layout)
-event, values = window.read()
-window.close()
-
-def cadastrar():
-    layout = [[sg.Text('Cadastro de Produto')],
-              [sg.Text('Produto', size=(15, 1)), sg.InputText()],
-              [sg.Text('Quantidade', size=(15, 1)), sg.InputText()],
-              [sg.Text('Preço', size=(15, 1)), sg.InputText()],
-              [sg.Submit('Cadastrar'), sg.Cancel('Cancelar')]]
+def estoque():
+    layout = [[sg.Text('Estoque')],
+    [sg.Table(values=contact_information, 
+    headings=['Produto','Quantidade','Preço'],
+    max_col_width=25, 
+    auto_size_columns=False,
+    justification='center', 
+    num_rows=20,
+    alternating_row_color='lightblue',
+    key='-TABLE-',
+    tooltip='This is a table')],
+    [sg.Button('Cadastrar'), sg.Button('Editar'), sg.Button('Excluir'), sg.Button('Sair')]]
+                
     window = sg.Window('+Supplier', layout)
     event, values = window.read()
     window.close()
     return event, values
+
+def cadastro_produto():
+    layout = [[sg.Text('Cadastro de produtos')],
+    [sg.Text('Nome do produto', size=(15,1)), sg.InputText()],
+    [sg.Text('Quantidade', size=(15,1)),sg.InputText()],
+    [sg.Text('Código', size=(15,1)), sg.InputText()],
+    [sg.Button('Cadastra'), sg.Button('Sair')]]
+
+    window = sg.Window('+Supplier', layout)
+    event, values = window.read()
+    window.close()
+    return event, values
+
+
+
 while True:
-    if event == "Cadastrar Produto":
-        cadastrar()
-        print(contact_information)
-        
-    elif event == sg.WIN_CLOSED or event == 'Voltar':
+    event, values = estoque()
+    if event == 'Cadastrar':
+        event, values = cadastro_produto()
+        if event == 'Cadastra':
+            produto.append(values[0])
+            quantidade.append(values[1])
+            preco.append(values[2])
+            contact_information.append([produto[0],quantidade[0],preco[0]])
+            produto.pop(0)
+            quantidade.pop(0)
+            preco.pop(0)
+    elif event == 'Editar':
+        sg.popup('Em construção')
+    elif event == 'Excluir':
+        sg.popup('Em construção')
+    elif event == 'Sair' or event == sg.WIN_CLOSED:
         break

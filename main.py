15123b5a20d29f9095da1338706af81cdc7cc9c1
Estima01 <matheus.estima@gmail.com>
2022-10-27 01:11:26 -0300

    retirada do pdv e gerador de codigo de barra

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from asyncio.windows_events import NULL
 import PySimpleGUI as sg
 import sqlite3 as sql
+from barcode import EAN13
+from barcode.writer import ImageWriter
 
 def create_table(): #criação da tabela de estoque
     conn = sql.connect('estoque.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS estoque(codigo INTEGER, produto TEXT, quantidade INTEGER, preco REAL)')
+    c.execute('CREATE TABLE IF NOT EXISTS estoque(codigo INTEGER, produto TEXT, quantidade INTEGER, preco_unitario REAL, preco_total REAL)')
     conn.commit()
     conn.close()
 
@@ -18,39 +20,29 @@ def view(): #visualização da tabela de estoque
     conn.close()
     return rows
 
-def carrinho():
-    conn = sql.connect('carrinho.db')
+def valor():
+    conn = sql.connect('estoque.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS carrinho(codigo INTEGER, produto TEXT, quantidade INTEGER, preco REAL)')
-    conn.commit()
-    conn.close()
-def view_carrinho():
-    conn = sql.connect('carrinho.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM carrinho')
+    c.execute('SELECT SUM(preco_total) FROM estoque')
     rows = c.fetchall()
     conn.close()
-    return rows
-def add_carrinho():
-    conn = sql.connect('carrinho.db')
-    c = conn.cursor()
-    c.execute('INSERT INTO carrinho VALUES(?,?,?,?)')
-    conn.commit()
-    conn.close()
+    for i in rows:
+        return i[0]
+
 #--------------------------------------------------------------#
 
 def estoque(): #tela do estoque
     layout = [[sg.Text('Estoque')],
     [sg.Table(values=view(), 
-    headings=['Código','Produto','Quantidade','Preço'],
+    headings=['Código','Produto','Quantidade','Preço Unitário','Preço Total'],
     max_col_width=25, 
     auto_size_columns=False,
     justification='center', 
     num_rows=20,
     key='-TABLE-',
     tooltip='This is a table')],
-    [sg.Text('Total: R$') ,sg.Text(valor(), size=(15,1), key='-TOTAL-')],
-    [sg.Button('Cadastrar'), sg.Button('Editar'), sg.Button('Excluir'), sg.Button('Sair'), sg.Button('PDV')]]
+    [sg.Text('Total em mercadoria: R$') ,sg.Text(valor(), size=(15,1), key='-TOTAL-')],
+    [sg.Button('Cadastrar'), sg.Button('Editar'), sg.Button('Excluir'), sg.Button('Gerar código de barras'),sg.Button('Sair')]]
                 
     window = sg.Window('+Supplier', layout)
     event, values = window.read()
@@ -93,52 +85,27 @@ def editar(): #tela de edição de itens no estoque
     window.close()
     return event, values
 
-def ponto_de_venda():
-    layout = [[sg.Text('Ponto de Venda')],
+def gerar_codigo(): #tela de geração de código de barras
+    layout = [[sg.Text('Gerar código de barras')],
     [sg.Text('Código do produto', size=(15,1)), sg.InputText()],
-    [sg.Text('Quantidade', size=(15,1)), sg.InputText()],
-    [sg.Table(values=view(),
-    headings=['Código','Produto','Quantidade','Preço'], 
-    max_col_width=25,
-    auto_size_columns=False, 
-    justification='center', 
-    num_rows=10, key='-TABLE-', 
-    tooltip='This is a table')],
-    [sg.Button('Adicionar ao carrinho')],
-    [sg.Text('Carrinho')],
-    [sg.Table(values=view_carrinho(),
-    headings=['Código','Produto','Quantidade','Preço'], 
-    max_col_width=25,
-    auto_size_columns=False, 
-    justification='center', 
-    num_rows=10, key='-TABLE-', 
-    tooltip='This is a table')],
-    [sg.Button('Vender'), sg.Button('Sair')],
-    [sg.Text('Total: R$') ,sg.Text('0,00', size=(15,1), key='-TOTAL-')]] 
+    [sg.Button('Gerar'), sg.Button('Sair')]]
 
     window = sg.Window('+Supplier', layout)
     event, values = window.read()
     window.close()
     return event, values
 
-def valor():
-    conn = sql.connect('estoque.db')
-    c = conn.cursor()
-    c.execute('SELECT SUM(preco) FROM estoque')
-    rows = c.fetchall()
-    conn.close()
-    return rows
-
 #Menu principal
 
 while True:
+    create_table()
     event, values = estoque()
     if event == 'Cadastrar':
         event, values = cadastro_produto()
         if event == 'Cadastra':
             conn = sql.connect('estoque.db')
             c = conn.cursor()
-            c.execute('INSERT INTO estoque VALUES (:codigo, :produto, :quantidade, :preco)', {'codigo': values[0], 'produto': values[1], 'quantidade': values[2], 'preco': values[3]})
+            c.execute('INSERT INTO estoque VALUES (:codigo, :produto, :quantidade, :preco_unitario, :preco_total)', {'codigo': values[0], 'produto': values[1], 'quantidade': values[2], 'preco_unitario': values[3], 'preco_total': float(values[2]) * float(values[3])})
             conn.commit()
             conn.close()
             sg.popup('Produto cadastrado com sucesso')
@@ -160,29 +127,14 @@ while True:
             conn.commit()
             conn.close()
             sg.popup('Produto excluído com sucesso')
-    elif event == 'PDV':
-        carrinho()
-        event, values = ponto_de_venda()
-        if event == 'Vender':
-            conn = sql.connect('estoque.db')
-            c = conn.cursor()
-            c.execute('SELECT quantidade FROM estoque WHERE codigo = :codigo', {'codigo': values[0]})
-            rows = c.fetchall()
-            conn.close()
-            if int(values[1]) > rows[0][0]:
-                sg.popup('Quantidade indisponível')
-            else:
-                conn = sql.connect('estoque.db')
-                c = conn.cursor()
-                c.execute('UPDATE estoque SET quantidade = :quantidade WHERE codigo = :codigo', {'codigo': values[0], 'quantidade': rows[0][0] - int(values[1])})
-                conn.commit()
-                conn.close()
-                sg.popup('Venda realizada com sucesso')
-        elif event == 'Adicionar ao Carrinho':
-            sg.popup('Em desenvolvimento')
-            ponto_de_venda()
-        elif event == 'Sair':
-            break
+    elif event == 'Gerar código de barras':
+        event, values = gerar_codigo()
+        if event == 'Gerar':
+            codigo_barras = EAN13(values[0], writer=ImageWriter())
+            codigo_barrastransformado = codigo_barras.save("codigo")
+
+            sg.popup('Código de barras gerado com sucesso')
+
     elif event == 'Sair' or event == sg.WIN_CLOSED:
         from login import *
         break
